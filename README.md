5b1ab45d5f8bd4ce149cd981a99f6a364e94771b
guido57 <guido@dogsally.com>
2024-05-25 15:03:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# Richonguzman / CA2RXU LoRa APRS iGate/Digirepeater
+
+This firmware is for using ESP32 based boards with LoRa Modules and GPS to live in the APRS world.
+
+![Screenshot](https://github.com/richonguzman/LoRa_APRS_iGate/blob/main/images/iGateOledScreen.jpeg)
+
+__(NOTE: This iGate Firmware was develop to work with all LoRa APRS Trackers and specially with this firmware <a href="https://github.com/richonguzman/LoRa_APRS_Tracker" target="_blank">LoRa APRS Tracker</a>)__
+
+___________________________________________________
+
+## You can support this project to continue to grow:
+
+[<img src="https://github.com/richonguzman/LoRa_APRS_Tracker/blob/main/images/github-sponsors.png">](https://github.com/sponsors/richonguzman)     [<img src="https://github.com/richonguzman/LoRa_APRS_Tracker/blob/main/images/paypalme.png">](http://paypal.me/richonguzman)
+
+____________________________________________________
+
+# WEB INSTALLER
+
+As easy as it gets, the new <a href="https://sq2cpa.github.io/lora-flasher/ca2rxu.html" target="_blank">Web Installer</a> (thanks Damian SQ2CPA)
+
+[<img src="https://github.com/richonguzman/LoRa_APRS_iGate/blob/main/images/WebFlasher.png">](https://sq2cpa.github.io/lora-flasher/ca2rxu.html)
+
+____________________________________________________
+
+# SUPPORTED BOARDS
+
+- TTGO Lilygo LoRa32 v2.1 / v1.6 (they work the same)
+
+- TTGO T-Beam v1.0 , v1.1, v1.2 (also variations with SX1262 and SX1268 LoRa Modules)
+
+- HELTEC V2, V3 , Wireless Stick, HT-CT62, Wireless Tracker
+
+- ESP32 Wroom + SX1278 LoRa Module or 400M30S 1W LoRa Module for a DIY Versions.
+
+- ESP32 + 4G/LTE A7670 Modem + SX1278 DIY Version.
+
+- Wemos Lolin32 Oled + SX1278 DIY Version.
+
+
+### Buying links --> <a href="https://github.com/richonguzman/LoRa_APRS_iGate/wiki/Z.-------Supported-Boards-and-Buying-Links" target="_blank">here</a>.
+
+____________________________________________________
+
+# WIKI
+
+### Installation Guide --> <a href="https://github.com/richonguzman/LoRa_APRS_iGate/wiki/01.-Installation-Guide" target="_blank">here</a>.
+
+*(Wiki has all configuration explanation, supported boards list, adding BME/BMP Wx modules and more)*
+
+____________________________________________________
+## Timeline (Versions):
+
+- 2024.05.23 Forced Reboot Mode added.
+- 2024.05.22 Experimental backup-Digirepeater-Mode when "only" iGate mode loses WiFi connection added.
+- 2024.05.20 WebConfig update to control wheter Messages and Objects should be Tx to RF.
+- 2024.05.17 HELTEC Wireless Stick Lite v3 support added.
+- 2024.05.14 BME modules will be autodetected (I2C Address and if it is BME280/BMP280/BME680).
+- 2024.05.13 PacketBuffer for Rx (25 Seg).
+- 2024.05.11 HELTEC Wireless Tracker support added.
+- 2024.04.23 T-LoRa32 v1.6/v2.1 with 915MHz support added.
+- 2024.04.23 ESP32 + 4G/LTE MODEM A7670SA + LoRa (SX1278) support added.
+- 2024.04.22 Wemos Lolin32 OLED DIY LoRa support added .
+- 2024.04.21 WEB INSTALLER (thanks Damian SQ2CPA).
+- 2024.04.20 New Output Buffer process: no more packets lost.
+- 2024.04.13 Received Packets added on WebUI.
+- 2024.04.09 iGate/Digirepeater own GPS beacon is encoded (Base91) now.
+- 2024.03.18 OE5HWN MeshCom board support added.
+- 2024.02.25 New Web Configuration UI with WiFi AP (thanks Damian SQ2CPA).
+- 2023.01.28 Updated to ElegantOTA v.3 (AsyncElegantOTA was deprecated).
+- 2024.01.19 TextSerialOutputForApp added to get text from Serial-Output over USB into PC for PinPoint App (https://www.pinpointaprs.com) and APRSIS32 App (http://aprsisce.wikidot.com)
+- 2024.01.12 Added iGate Mode to also repeat packets (like a iGate+DigiRepeater) in stationMode 2 and 5.
+- 2024.01.11 Added iGate Mode to enable APRS-IS and LoRa beacon report at the same time.
+- 2024.01.05 Lilygo TTGO T-Beam V1, V1.2, V1 + SX1268, V1.2 + SX1262 support added.
+- 2024.01.02 EByte 400M30S 1 Watt LoRa module for DIY ESP32 iGate support added.
+- 2023.12.27 HELTEC V3 board support added. Thanks Luc ON2ON.
+- 2023.12.26 BME680 module support added.
+- 2023.12.07 MIC-E process and Syslog added.
+- 2023.12.06 HELTEC V2 board support added.
+- 2023.11.26 Small correction to enable Syslog in stationMode5.
+- 2023.10.09 Added "WIDE1-1" to Tx packets from iGate to be *repeated* by Digirepeaters.
+- 2023.10.09 BMP280 module support added.
+- 2023.08.20 Added External Voltage Measurement (Max 15V!)
+- 2023.08.05 Ground Height Correction for Pressure readings added.
+- 2023.07.31 StationMode5 added: iGate when WiFi and APRS available, DigiRepeater when not.
+- 2023.07.16 Small OTA, BME module update.
+- 2023.07.05 Adding monitor info of Battery connected.
+- 2023.06.18 Info on Oled Screen mayor update, added RSSI and Distance to Listened Station.
+- 2023.06.17 BME280 Module (Temperature, Humidity, Pressure) support added.
+- 2023.06.12 Syslog added.
+- 2023.06.10 OTA update support for Firmware and Filesystem.
+- 2023.06.08 Adding Digirepeater Functions.
+- 2023.06.06 Full repack of Code and adding _enableTx_ only for Ham Ops.
+- 2023.05.23 Processing Query's from RF/LoRa or APRS-IS (Send "Help" Message to test).
+- 2023.05.19 Saving Last-Heard Stations for validating Tx Responses.
+- 2023.05.12 Tx Packet from APRS-IS to LoRa-RF Correction.
+- 2023.03.01 Tx Packet from APRS-IS to nearby LoRa Stations.
+- 2023.02.17 Receiving Feed from APRS-IS.
+- 2023.02.10 First Beta (receiving LoRa Beacon/Packets and uploading to APRS-IS).
+
+__________________________________________
+
+# Hope You Enjoy this, 73 !!  CA2RXU , Valparaiso, Chile
---
+++ src/utils.cpp
@@ -0,0 +1,280 @@
+#include <WiFi.h>
+#include "configuration.h"
+#include "station_utils.h"
+#include "battery_utils.h"
+#include "aprs_is_utils.h"
+#include "boards_pinout.h"
+#include "syslog_utils.h"
+#include "A7670_utils.h"
+#include "wifi_utils.h"
+#include "gps_utils.h"
+#include "bme_utils.h"
+#include "display.h"
+#include "utils.h"
+
+extern Configuration        Config;
+extern WiFiClient           espClient;
+extern String               versionDate;
+extern String               firstLine;
+extern String               secondLine;
+extern String               thirdLine;
+extern String               fourthLine;
+extern String               fifthLine;
+extern String               sixthLine;
+extern String               seventhLine;
+extern String               iGateBeaconPacket;
+extern String               iGateLoRaBeaconPacket;
+extern std::vector<String>  lastHeardStation;
+extern int                  rssi;
+extern float                snr;
+extern int                  freqError;
+extern String               distance;
+extern uint32_t             lastWiFiCheck;
+extern bool                 WiFiConnect;
+extern bool                 WiFiConnected;
+extern int                  wxModuleType;
+extern bool                 backUpDigiMode;
+
+bool        statusAfterBoot     = true;
+bool        beaconUpdate        = true;
+uint32_t    lastBeaconTx        = 0;
+uint32_t    lastScreenOn        = millis();
+
+
+namespace Utils {
+
+    void processStatus() {
+        String status = Config.callsign + ">APZMDM," + Config.beacon.path;
+        
+        if (WiFi.status() == WL_CONNECTED && Config.aprs_is.active && Config.beacon.sendViaAPRSIS) {
+            delay(1000);
+            status += ",qAC:>https://github.com/guido57/LoRa_APRS_iGate " + versionDate;
+            APRS_IS_Utils::upload(status);
+            SYSLOG_Utils::log(2, status,0,0,0);   // APRSIS TX
+            statusAfterBoot = false;
+        }
+        if (statusAfterBoot && !Config.beacon.sendViaAPRSIS && Config.beacon.sendViaRF) {
+            delay(2000);
+            status += ":>https://github.com/guido57/LoRa_APRS_iGate " + versionDate;
+            STATION_Utils::addToOutputPacketBuffer(status);
+            statusAfterBoot = false;
+        }
+    }
+
+    String getLocalIP() {
+        if (!WiFiConnected) {
+            return "IP :  192.168.4.1";
+        } else if (backUpDigiMode) {
+            return "- BACKUP DIGI MODE -";
+        } else {
+            return "IP :  " + String(WiFi.localIP()[0]) + "." + String(WiFi.localIP()[1]) + "." + String(WiFi.localIP()[2]) + "." + String(WiFi.localIP()[3]);
+        }        
+    }
+
+    void setupDisplay() {
+        setup_display();
+        #ifdef INTERNAL_LED_PIN
+            digitalWrite(INTERNAL_LED_PIN,HIGH);
+        #endif
+        Serial.println("\nStarting Station: " + Config.callsign + "   Version: " + versionDate);
+        show_display(" LoRa APRS", "", "   ( iGATE & DIGI )", "", "", " IW5ALZ ", "      " + versionDate, 4000);
+        #ifdef INTERNAL_LED_PIN
+            digitalWrite(INTERNAL_LED_PIN,LOW);
+        #endif
+        firstLine   = Config.callsign;
+        seventhLine = "     listening...";
+    }
+
+    void activeStations() {
+        fourthLine = "Stations (" + String(Config.rememberStationTime) + "min) = ";
+        if (lastHeardStation.size() < 10) {
+            fourthLine += " ";
+        }
+        fourthLine += String(lastHeardStation.size());            
+    }
+
+    void checkBeaconInterval() {
+        uint32_t lastTx = millis() - lastBeaconTx;
+        String beaconPacket             = iGateBeaconPacket;
+        String secondaryBeaconPacket    = iGateLoRaBeaconPacket;
+
+        if (lastBeaconTx == 0 || lastTx >= Config.beacon.interval * 60 * 1000) {
+            beaconUpdate = true;    
+        }
+
+        if (beaconUpdate) {
+            if (!Config.display.alwaysOn && Config.display.timeout != 0) {
+                display_toggle(true);
+            }
+            Utils::println("-- Sending Beacon to APRSIS --");
+
+            STATION_Utils::deleteNotHeard();
+
+            activeStations();
+
+            if (Config.bme.active && wxModuleType != 0) {
+                String sensorData = BME_Utils::readDataSensor();
+                beaconPacket += sensorData;
+                secondaryBeaconPacket += sensorData;
+            } else if (Config.bme.active && wxModuleType == 0) {
+                beaconPacket += ".../...g...t...r...p...P...h..b.....";
+                secondaryBeaconPacket += ".../...g...t...r...p...P...h..b.....";
+            }
+            beaconPacket += Config.beacon.comment;
+            secondaryBeaconPacket += Config.beacon.comment;
+
+            #ifdef BATTERY_PIN
+                if (Config.sendBatteryVoltage) {
+                    String batteryInfo = "Batt=" + String(BATTERY_Utils::checkBattery(),2) + "V";
+                    beaconPacket += " " + batteryInfo;
+                    secondaryBeaconPacket += " " + batteryInfo;
+                    sixthLine = "     ( " + batteryInfo + ")";
+                }
+            #endif
+
+            if (Config.externalVoltageMeasurement) {
+                String externalVoltage = String(BATTERY_Utils::checkExternalVoltage(),2) + "V";
+                beaconPacket += " Ext=" + externalVoltage;
+                secondaryBeaconPacket += " Ext=" + externalVoltage;
+                sixthLine = "    (Ext V=" + externalVoltage;
+            }
+
+            if (Config.aprs_is.active && Config.beacon.sendViaAPRSIS && !backUpDigiMode) {
+                show_display(firstLine, secondLine, thirdLine, fourthLine, fifthLine, sixthLine, "SENDING IGATE BEACON", 0); 
+                seventhLine = "     listening...";
+                #ifdef ESP32_DIY_LoRa_A7670
+                    A7670_Utils::uploadToAPRSIS(beaconPacket);
+                #else
+                    APRS_IS_Utils::upload(beaconPacket);
+                #endif
+            }
+
+            if (Config.beacon.sendViaRF || backUpDigiMode) {
+                show_display(firstLine, secondLine, thirdLine, fourthLine, fifthLine, sixthLine, "SENDING DIGI BEACON", 0);
+                seventhLine = "     listening...";
+                STATION_Utils::addToOutputPacketBuffer(secondaryBeaconPacket);
+            }
+
+            lastBeaconTx = millis();
+            lastScreenOn = millis();
+            beaconUpdate = false;
+        }
+
+        if (statusAfterBoot) {
+            processStatus();
+        }
+    }
+
+    void checkDisplayInterval() {
+        uint32_t lastDisplayTime = millis() - lastScreenOn;
+        if (!Config.display.alwaysOn && lastDisplayTime >= Config.display.timeout * 1000) {
+            display_toggle(false);
+        }
+    }
+
+    void checkWiFiInterval() {
+        uint32_t WiFiCheck = millis() - lastWiFiCheck;
+        if (WiFi.status() != WL_CONNECTED && WiFiCheck >= 15 * 60 * 1000) {
+            WiFiConnect = true;
+        }
+        if (WiFiConnect) {
+            Serial.println("\nConnecting to WiFi ...");
+            WIFI_Utils::startWiFi();
+            lastWiFiCheck = millis();
+            WiFiConnect = false;
+        }
+    }
+
+    void validateFreqs() {
+        if (Config.loramodule.txFreq != Config.loramodule.rxFreq && abs(Config.loramodule.txFreq - Config.loramodule.rxFreq) < 125000) {
+            Serial.println("Tx Freq less than 125kHz from Rx Freq ---> NOT VALID");
+            show_display("Tx Freq is less than ", "125kHz from Rx Freq", "device will autofix", "and then reboot", 1000);
+            Config.loramodule.txFreq = Config.loramodule.rxFreq; // Inform about that but then change the TX QRG to RX QRG and reset the device
+            Config.writeFile();
+            ESP.restart();
+        }
+    }
+
+    void typeOfPacket(const String& packet, uint8_t packetType) {
+        String sender;
+        switch (packetType) {
+            case 0: // LoRa-APRS
+                fifthLine = "LoRa Rx ----> APRS-IS";
+                sender = packet.substring(0,packet.indexOf(">"));
+                break;
+            case 1: // APRS-LoRa
+                fifthLine = "APRS-IS ----> LoRa Tx";
+                sender = packet.substring(0,packet.indexOf(">"));
+                break;
+            case 2: // Digi
+                fifthLine = "LoRa Rx ----> LoRa Tx";
+                sender = packet.substring(0,packet.indexOf(">"));
+                break;
+        }
+        for (int i = sender.length(); i < 9; i++) {
+            sender += " ";
+        }
+        if (packet.indexOf("::") >= 10) {
+            sixthLine = sender + "> MESSAGE";
+            seventhLine = "RSSI:" + String(rssi) + "dBm SNR: " + String(snr) + "dBm";
+        } else if (packet.indexOf(":>") >= 10) {
+            sixthLine = sender + "> NEW STATUS";
+            seventhLine = "RSSI:" + String(rssi) + "dBm SNR: " + String(snr) + "dBm";
+        } else if (packet.indexOf(":!") >= 10 || packet.indexOf(":=") >= 10) {
+            sixthLine = sender + "> GPS BEACON";
+            if (!Config.syslog.active) {
+                GPS_Utils::getDistanceAndComment(packet);       // to be checked!!!
+            }            
+            seventhLine = "RSSI:" + String(rssi) + "dBm";
+            if (rssi <= -100) {
+                seventhLine += " ";
+            } else {
+                seventhLine += "  ";
+            }
+            if (distance.indexOf(".") == 1) {
+                seventhLine += " ";
+            }
+            seventhLine += "D:" + distance + "km";
+        } else if (packet.indexOf(":T#") >= 10 && packet.indexOf(":=/") == -1) {
+            sixthLine = sender + "> TELEMETRY";
+            seventhLine = "RSSI:" + String(rssi) + "dBm SNR: " + String(snr) + "dBm";
+        } else if (packet.indexOf(":`") >= 10) {
+            sixthLine = sender + ">  MIC-E";
+            seventhLine = "RSSI:" + String(rssi) + "dBm SNR: " + String(snr) + "dBm";
+        } else if (packet.indexOf(":;") >= 10) {
+            sixthLine = sender + ">  OBJECT";
+            seventhLine = "RSSI:" + String(rssi) + "dBm SNR: " + String(snr) + "dBm";
+        } else {
+            sixthLine = sender + "> ??????????";
+            seventhLine = "RSSI:" + String(rssi) + "dBm SNR: " + String(snr) + "dBm";
+        }
+    }
+
+    void print(const String& text) {
+        if (!Config.tnc.enableSerial) {
+            Serial.print(text);
+        }
+    }
+
+    void println(const String& text) {
+        if (!Config.tnc.enableSerial) {
+            Serial.println(text);
+        }
+    }
+
+    void checkRebootMode() {
+        if (Config.rebootMode && Config.rebootModeTime > 0) {
+            Serial.println("(Reboot Time Set to " + String(Config.rebootModeTime) + " hours)");
+        }
+    }
+
+    void checkRebootTime() {
+        if (Config.rebootMode && Config.rebootModeTime > 0) {
+            if (millis() > Config.rebootModeTime * 60 * 60 * 1000) {
+                Serial.println("\n*** Automatic Reboot Time Restart! ****\n");
+                ESP.restart();
+            }
+        }
+    }
+
+}
